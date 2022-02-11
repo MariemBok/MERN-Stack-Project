02db7b38f807b7ef9d225d0c4f6cc2831d18062a
Mariem Bokri <mariem.bokri@esprit.tn>
2022-02-11 10:04:52 +0100

    props and usestate

--- first_react_project/src/Tweet.js
+++ first_react_project/src/Tweet.js
@@ -1,16 +1,18 @@
 import React, {useState} from "react";
 
 function Tweet({name}){
+    const [isRed, redSet] = useState(false);
     const [count, countSet]= useState(0);
     const Increment = () =>{
         countSet(count + 1);
+        redSet(!isRed);
     }
 
     return(
         <div className="tweet">
             <h2>{name}</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis alias provident dolores?</p>
-            <h3>well, well, well.</h3>
+            <h3 className={isRed ? "red" : "blue"}>well, well, well.</h3>
             <button onClick={Increment}>Increment</button>
             <h3>{count}</h3>
         </div>
