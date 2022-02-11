3154da27b11bb2a4d7c0e68544c75e7f766d96ed
Mariem Bokri <mariem.bokri@esprit.tn>
2022-02-11 12:01:14 +0100

    testing out styled-components

--- first_react_project/src/App.js
+++ first_react_project/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Tweet from "./Tweet";
 import Clock from "./Clock";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
+  const Title = styled.h3`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+  const Mariem = styled.p`
+  color: violet;  
+  `;
   return (
     <div className="app">
-      <Clock/>
+      <Title>This is the title</Title>
+      <Mariem>Mariem, eh?</Mariem>
       <Tweet name="Mariem"/>
       <Tweet name="Ali"/>
       <Tweet name="Mohssen"/>
